956a0f68344b6cff594fbd2ccd1bfe3238415795
luanillustrates <luanillustrates@gmail.com>
2023-07-23 22:26:12 +0930

    updated js

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,29 @@
+var city = document.getElementById("search-city");
+var searchCity = document.getElementById("search-button");
+var clearSearch = document.getElementById("clear-history");
+var historyEl = document.getElementById("history");
+var currentWeather = document.getElementById("current-weather");
+var cityName = document.getElementById("city-name");
+var currentImage = document.getElementById("current-img");
+var currentTemp = document.getElementById("temperature");
+var currentHumidity = document.getElementById("humidity");
+var currentUV = document.getElementById("UV");
+var currentWind = document.getElementById("wind");
+var fiveDay = document.getElementById("fiveday");
+let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
 const apiKey = "";
-var city = document.getElementById("search-city")
-var searchEl = document.getElementById("search-button")
 
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+function getWeather(cityName) {
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
+    $.get(queryURL)
+        .then(function (response) {
+            currentWeather.classList.remove("d-none");
+        }
+        
+        )
+}
+
+
 
 fetch(queryURL)
 
